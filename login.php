953d19eabded544a27eb09639e1a3ecbda352f05
Wannapon Rotjanarungtawee <40911600+Wannapon@users.noreply.github.com>
2019-05-15 14:15:40 +0700

    ++img(background,icon)
    ++

--- login.php
+++ login.php
@@ -1,35 +1,27 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Login </title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Plant Disease</title>
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-    <!-- <script src="bootstrap/js/bootstrap.min.js" ></script> -->
+    <link rel="stylesheet" href="bootstrap/css/style.css">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link rel="shortcut icon" href="img/leaficon.ico" type="image/x-icon" />
+
 </head>
+
 <body>
-    <div class="container"><br>
-    <center><h4> Login . <a href="index.php">Index</a></h4></center> <hr>
-        <form method="post" action="ConnData/check_login.php">
-            <div class="row">
-                <div class="col-4"></div>
-                <div class="col-4">
-                    <label id="first"> First name :</label><br/>
-                    <input class="form-control" type="text" name="firstname" required><br/>
-                    <label id="first">Password :</label><br/>
-                    <input class="form-control" type="password" name="password" required><br/>
-
-                    <div class="row">
-                        <div class="col-9"></div>
-                        <input class="col-3 form-control btn-primary" type="submit" name="Submit" value="Login">
-                    </div>
-                </div>
-                <div class="col-4"></div>
-            </div>
-        
-        </form>
-    </div>
-
+    
+    <form class="box" action="login.php" method="post">
+        <h1>LOGIN</h1>
+        <input type="text" placeholder="Username" maxlength="20">
+        <input type="password" placeholder="Password">
+        <input type="submit" value="Login">
+    </form>
+    
 </body>
+
 </html>
