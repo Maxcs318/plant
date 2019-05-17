faef36ac04961e3e710399864aa33e080c2eb233
Wannapon Rotjanarungtawee <40911600+Wannapon@users.noreply.github.com>
2019-05-17 11:32:20 +0700

    ++ Post : script
    ++

--- Posts/post_form.php
+++ Posts/post_form.php
@@ -1,36 +1,48 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-    <meta charset="utf-8"/>
+    <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Test Up Image</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../bootstrap/css/style.css">
+    <link rel="shortcut icon" href="../img/leaficon.ico" type="image/x-icon" />
 </head>
-<body>
-<?php session_start(); ?>
-    <div class="container">
-        <form action="../ConnData/InsertPost.php" method="post" enctype="multipart/form-data"> 
-        <div class="row">
-            <div class="col-3">                
-            </div>
-            <div class="col-6">
-                <br>
-                <center><h4>Post.</h4></center>
-                <label id="first">Header :</label><br/>
-                <input class="form-control" type="text" name="header" required><br/>
-                <label id="first">Detale :</label><br/>
-                <input class="form-control" type="text" name="detail" required><br/>
 
-                <label id="first">Upload Photo :</label><br/>
-                <input type="file" name="image[]" id="image" > <br><br>
-                <label id="first">Upload Photo 2 :</label><br/>
-                <input type="file" name="image[]" id="image" >
-                <input type="hidden" name="own" value="<?php echo $_SESSION["m_id"]; ?>">
-                <input type="hidden" name="date" value="<?php echo date("Y-m-d H:i:s",time()+ (60*60)*5 ); ?>">
-                <br>
-                <?php 
-                    function generateRandomString($length = 30) {
+<body>
+    <?php session_start(); ?>
+    <div class="container">
+        <form action="../ConnData/InsertPost.php" method="post" enctype="multipart/form-data">
+            <div class="row">
+                <div class="col-3">
+                </div>
+                <div class="col-6">
+                    <br>
+                    <center>
+                        <h4>Post.</h4>
+                    </center>
+                    <label id="first">Header :</label><br />
+                    <input class="form-control" type="text" name="header" required><br />
+                    <label id="first">Detale :</label><br />
+                    <input class="form-control" type="text" name="detail" required><br />
+
+                    <label id="first">Upload Photo :</label><br />
+                    <input type="file" name="image[]" id="image"> <br><br>
+                    <label id="first">Upload Photo 2 :</label><br />
+                    <input type="file" name="image[]" id="image">
+                    <input type="hidden" name="own" value="<?php echo $_SESSION["m_id"]; ?>">
+                    <input type="hidden" name="date" value="<?php echo date("Y-m-d H:i:s", time() + (60 * 60) * 5); ?>">
+                    <br>
+                    <!-- ++ button ++ -->
+                    <button id="btn-sayhi">
+                        +
+                    </button>
+                    <!-- ++ button ++ -->
+                    <?php
+                    function generateRandomString($length = 30)
+                    {
                         $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
                         $charactersLength = strlen($characters);
                         $randomString = '';
@@ -40,16 +52,63 @@
                         return $randomString;
                     }
                     // echo generateRandomString();
-                ?>
-                <input type="hidden" name="key_post_image" value="<?php echo generateRandomString(); ?>">
+                    ?>
+                    <input type="hidden" name="key_post_image" value="<?php echo generateRandomString(); ?>">
 
-                <button  class="btn-primary form-control col-3" id="insert" type="submit" name="save" style="float: right;">Post.</button>
-            </div>  
-            <div class="col-3">
+                    <button class="btn-primary form-control col-3" id="insert" type="submit" name="save" style="float: right;">Post.</button>
+                </div>
+                <div class="col-3">
+                </div>
+
             </div>
-            
-        </div>
         </form>
     </div>
+
+    <!-- script + function -->
+    <script>
+        $(document).ready(function() {
+            $('#btn-show').click(function() {
+                personData.forEach(function(data, i) {
+                    $('#table-data tbody').append(
+                        '<tr>' +
+                        '<td>' + (i + 1) + '</td>' +
+                        '<td>' + data.name + '</td>' +
+                        '<td>' + data.age + '</td>' +
+                        '<td>' + data.gender + '</td>' +
+                        '<td>' + data.address.city + '</td>' +
+                        '<td>' + data.address.country + '</td>' +
+                        '</tr>'
+                    )
+                });
+            });
+
+            $('h1').append('<i> Append ,</i>');
+            $('h1').prepend('<i> Prepend ,</i>');
+            $('h1').after('<i> After ,</i>');
+            $('h1').before('<i> Before ,</i>');
+
+            $('#btn-sayhi').click(function() {
+                $('#show-data').append('<li> + </li>');
+
+            });
+            $('#btn-last').click(function() {
+                $("#show-data li").last().remove();
+
+            });
+            $('#btn-remove').click(function() {
+                $("#show-data li").remove();
+            });
+
+            $('#btn-input').click(function() {
+                var name = $('#first').val();
+                var lastname = $('#last').val();
+                var age = $('#age').val();
+                $('#show-input').text('Name : ' + name + ' , LastName : ' + lastname + ' , Age : ' + age);
+            });
+
+        });
+    </script>
+
 </body>
+
 </html>
--- register.php
+++ register.php
@@ -18,68 +18,74 @@
 
     <div class="container">
         <!-- register tab -->
-        <div class="tab" style="margin-top: 40px;">
+        <div class="tab">
             <button class="tablinks active" onclick="openCity(event, 'user')">User Register</button>
             <button class="tablinks" onclick="openCity(event, 'expert')">Expert Register</button>
         </div>
         <!-- end register tab -->
-        <form action="ConnData/InsertRegister.php" method="post">
 
-            <div class="row">
-                <!-- user register -->
-                <div id="user" class="col-xs-12 col-lg-12 box">
-                    <h1>User Register</h4>
-                        <p>First Name</p>
-                        <input class="form-control" type="text" name="firstname" placeholder="first name" maxlength="25" required>
+        <!-- user register -->
+            <form action="ConnData/InsertRegister.php" method="post">
+                <div class="row">
+                    <div id="user" class="col-xs-4 col-lg-12 box">
+                        <h1>User Register</h4>
+                            <p>First Name</p>
+                            <input class="form-control" type="text" name="firstname" placeholder="first name" maxlength="25" required>
 
-                        <p>Last Name</p>
-                        <input class="form-control" type="text" name="lastname" placeholder="last name" maxlength="25" required>
+                            <p>Last Name</p>
+                            <input class="form-control" type="text" name="lastname" placeholder="last name" maxlength="25" required>
 
-                        <p>E-mail</p>
-                        <input class="form-control" type="email" name="email" placeholder="example@plant.com" required>
+                            <p>E-mail</p>
+                            <input class="form-control" type="email" name="email" placeholder="example@plant.com" required>
 
-                        <p>Phone</p>
-                        <input class="form-control" name='s_money' id='s_money' type='text' maxlength="10" placeholder="number 0-9" required onKeyUp="if(this.value*1!=this.value) this.value='' ;">
+                            <p>Phone</p>
+                            <input class="form-control" name='s_money' id='s_money' type='text' maxlength="10" placeholder="number 0-9" required onKeyUp="if(this.value*1!=this.value) this.value='' ;">
 
-                        <p>User Name</p>
-                        <input class="form-control" type="text" name="username" placeholder="username" maxlength="10" required>
+                            <p>User Name</p>
+                            <input class="form-control" type="text" name="username" placeholder="username" maxlength="10" required>
 
-                        <p>Password</p>
-                        <input class="form-control" type="password" name="password" placeholder="password" maxlength="10" required>
+                            <p>Password</p>
+                            <input class="form-control" type="password" name="password" placeholder="password" maxlength="10" required>
 
-                        <input type="hidden" name="status" value="user">
-                        <button id="insert" type="submit" name="save">Save</button>
+                            <input type="hidden" name="status" value="user">
+                            <button id="insert" type="submit" name="save">Save</button>
+                    </div>
                 </div>
-            </div>
-        </form>
-        <form action="ConnData/InsertRegister.php" method="post">
-            <!-- expert register -->
-            <div class="row">
-                <div id="expert" class="col-xs-12 col-lg-12 box" style="display: none">
-                    <h1>Expert Register</h4>
-                        <p>First Name</p>
-                        <input class="form-control" type="text" name="firstname" placeholder="first name" required>
+            </form>
 
-                        <p>Last Name</p>
-                        <input class="form-control" type="text" name="lastname" placeholder="last name" required>
+            <form action="ConnData/InsertRegister.php" method="post">
+                <div class="row">
+                    <div id="expert" class="col-xs-4 col-lg-12 box" style="display: none">
+                        <h1>Expert Register</h4>
+                            <p>First Name</p>
+                            <input class="form-control" type="text" name="firstname" placeholder="first name" required>
 
-                        <p>E-mail</p>
-                        <input class="form-control" type="email" name="email" placeholder="example@plant.com" required>
+                            <p>Last Name</p>
+                            <input class="form-control" type="text" name="lastname" placeholder="last name" required>
 
-                        <p>Phone</p>
-                        <input class="form-control" name='s_money' id='s_money' type='text' size='10' placeholder="number 0-9" required onKeyUp="if(this.value*1!=this.value) this.value='' ;">
+                            <p>E-mail</p>
+                            <input class="form-control" type="email" name="email" placeholder="example@plant.com" required>
 
-                        <p>User Name</p>
-                        <input class="form-control" type="text" name="username" placeholder="username" required>
+                            <p>Phone</p>
+                            <input class="form-control" name='s_money' id='s_money' type='text' size='10' placeholder="number 0-9" required onKeyUp="if(this.value*1!=this.value) this.value='' ;">
 
-                        <p>Password</p>
-                        <input class="form-control" type="password" name="password" placeholder="password" required>
+                            <p>Certificate</p>
+                            <input class="form-control" type="text" name="certificate" placeholder="certificate" required>
 
-                        <input type="hidden" name="status" value="expert">
-                        <button id="insert" type="submit" name="save">Save</button>
+                            <p>User Name</p>
+                            <input class="form-control" type="text" name="username" placeholder="username" required>
+
+                            <p>Password</p>
+                            <input class="form-control" type="password" name="password" placeholder="password" required>
+
+                            <input type="hidden" name="status" value="expert">
+                            <button id="insert" type="submit" name="save">Save</button>
+                    </div>
                 </div>
-        </form>
-    </div><!-- end container -->
+            </form>
+        <!-- expert register -->
+
+    </div>
 
     <!-- Togglable Script -->
     <script>
