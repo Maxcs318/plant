e24a0aca67de2e61a6f7fd164a8557a9be018cf0
Wannapon Rotjanarungtawee <40911600+Wannapon@users.noreply.github.com>
2019-05-17 17:23:13 +0700

    ++ style colum xs, md

--- register.php
+++ register.php
@@ -2,8 +2,7 @@
 <html lang="en">
 
 <head>
-    <script type="text/javascript" src="jquery.js"></script>
-    <script type="text/javascript" src="jquery.alphanumeric.js"></script>
+    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -18,76 +17,76 @@
 <body>
 
     <div class="container">
-        <!-- register tab -->
-        <div class="tab">
-            <button class="tablinks active" onclick="openCity(event, 'user')">User Register</button>
-            <button class="tablinks" onclick="openCity(event, 'expert')">Expert Register</button>
-        </div>
-        <!-- end register tab -->
 
         <!-- user register -->
-            <form action="ConnData/InsertRegister.php" method="post">
-                <div class="row">
-                    <div class="col-4"></div>
-                    <div id="user" class="col-xs-4 col-lg-4 box">
-                        <h1>User Register</h4>
-                            <p>First Name</p>
-                            <input class="form-control" type="text" name="firstname" placeholder="first name" maxlength="25" required>
+        <form action="ConnData/InsertRegister.php" method="post">
+            <div class="row">
+                <div class="col-xs-4 col-sm-4 col-md-4"></div>
+                
+                <div id="user" class="col-xs-4 col-sm-4 col-md-4 box">
+                
+                    <h1>User Register</h4>
+                    <button class="tablinks tab " onclick="openCity(event, 'expert')">Expert Register</button>
+                        <p>First Name</p>
+                        <input class="form-control" type="text" name="firstname" placeholder="first name" maxlength="25" required>
 
-                            <p>Last Name</p>
-                            <input class="form-control" type="text" name="lastname" placeholder="last name" maxlength="25" required>
+                        <p>Last Name</p>
+                        <input class="form-control" type="text" name="lastname" placeholder="last name" maxlength="25" required>
 
-                            <p>E-mail</p>
-                            <input class="form-control" type="email" name="email" placeholder="example@plant.com" required>
+                        <p>E-mail</p>
+                        <input class="form-control" type="email" name="email" placeholder="example@plant.com" required>
 
-                            <p>Phone</p>
-                            <input class="form-control" name='s_money' id='s_money' type='text' maxlength="10" placeholder="number 0-9" required onKeyUp="if(this.value*1!=this.value) this.value='' ;">
+                        <p>Phone</p>
+                        <input class="form-control" name='s_money' id='s_money' type='text' maxlength="10" placeholder="number 0-9" required onKeyUp="if(this.value*1!=this.value) this.value='' ;">
 
-                            <p>User Name</p>
-                            <input class="form-control" type="text" name="username" placeholder="username" maxlength="10" required>
+                        <p>User Name</p>
+                        <input class="form-control" type="text" name="username" placeholder="username" maxlength="10" required>
 
-                            <p>Password</p>
-                            <input class="form-control" type="password" name="password" placeholder="password" maxlength="10" required>
+                        <p>Password</p>
+                        <input class="form-control" type="password" name="password" placeholder="password" maxlength="10" required>
 
-                            <input type="hidden" name="status" value="user">
-                            <button id="insert" type="submit" name="save">Save</button>
-                    </div>
-                    <div class="col-4"></div>
+                        <input type="hidden" name="status" value="user">
+                        <button id="insert" type="submit" name="save">Save</button>
                 </div>
-            </form>
+                <div class="col-xs-4 col-sm-4 col-md-4"></div>
+            </div>
+        </form>
 
-            <form action="ConnData/InsertRegister.php" method="post">
-                <div class="row">
-                <div class="col-4"></div>
-                    <div id="expert" class="col-xs-4 col-lg-4 box" style="display: none">
-                        <h1>Expert Register</h4>
-                            <p>First Name</p>
-                            <input class="form-control" type="text" name="firstname" placeholder="first name" required>
+        <form action="ConnData/InsertRegister.php" method="post">
+            <div class="row">
+                
+                <div class="col-xs-4 col-sm-4 col-md-4"></div>
+                <div id="expert" class="col-xs-4 col-sm-4 col-md-4 box" style="display: none">
+                
+                    <h1>Expert Register</h4>
+                    <button class="tablinks tab  active" onclick="openCity(event, 'user')">User Register</button>
+                        <p>First Name</p>
+                        <input class="form-control" type="text" name="firstname" placeholder="first name" required>
 
-                            <p>Last Name</p>
-                            <input class="form-control" type="text" name="lastname" placeholder="last name" required>
+                        <p>Last Name</p>
+                        <input class="form-control" type="text" name="lastname" placeholder="last name" required>
 
-                            <p>E-mail</p>
-                            <input class="form-control" type="email" name="email" placeholder="example@plant.com" required>
+                        <p>E-mail</p>
+                        <input class="form-control" type="email" name="email" placeholder="example@plant.com" required>
 
-                            <p>Phone</p>
-                            <input class="form-control" name='s_money' id='s_money' type='text' size='10' placeholder="number 0-9" required onKeyUp="if(this.value*1!=this.value) this.value='' ;">
+                        <p>Phone</p>
+                        <input class="form-control" name='s_money' id='s_money' type='text' size='10' placeholder="number 0-9" required onKeyUp="if(this.value*1!=this.value) this.value='' ;">
 
-                            <p>Certificate</p>
-                            <input class="form-control" type="text" name="certificate" placeholder="certificate" required>
+                        <p>Certificate</p>
+                        <input class="form-control" type="text" name="certificate" placeholder="certificate" required>
 
-                            <p>User Name</p>
-                            <input class="form-control" type="text" name="username" placeholder="username" required>
+                        <p>User Name</p>
+                        <input class="form-control" type="text" name="username" placeholder="username" required>
 
-                            <p>Password</p>
-                            <input class="form-control" type="password" name="password" placeholder="password" required>
+                        <p>Password</p>
+                        <input class="form-control" type="password" name="password" placeholder="password" required>
 
-                            <input type="hidden" name="status" value="expert">
-                            <button id="insert" type="submit" name="save">Save</button>
-                    </div>
-                    <div class="col-4"></div>
+                        <input type="hidden" name="status" value="expert">
+                        <button id="insert" type="submit" name="save">Save</button>
                 </div>
-            </form>
+                <div class="col-xs-4 col-sm-4 col-md-4"></div>
+            </div>
+        </form>
         <!-- expert register -->
 
     </div>
