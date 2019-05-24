e8e7cb66c5dfc28e8865c706542d82d59e6d1ad3
Maxcs318 <maxkiller1112@gmail.com>
2019-05-24 10:28:09 +0700

    + status comment , confirm post
    ==

--- ConnData/InsertComment.php
+++ ConnData/InsertComment.php
@@ -5,6 +5,26 @@
     VALUES ('".$_POST['commentdetail']."','".$_POST['commentown']."','".$_POST['commentconfirm']."'
     ,'".$_POST['commentdate']."','".$_POST['id_linkpost']."')";
     if ($conn->query($sql) === TRUE) {
+        // header("location:../Posts/post_selected.php?getPostID=".$_POST['id_linkpost']);      
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    $conn->close();
+?>
+
+<?php require("connectDB.php");?>
+
+<?php
+    if($_POST['commentconfirm']!=''){
+        $sql = "UPDATE posts SET p_status_comment= 'comment' , p_status_confirm ='confirm' 
+        WHERE p_id='".$_POST['id_linkpost']."' ";
+    }
+    if($_POST['commentconfirm']==''){
+        $sql = "UPDATE posts SET p_status_comment= 'comment' 
+        WHERE p_id='".$_POST['id_linkpost']."' ";
+    }
+
+    if ($conn->query($sql) === TRUE) {
         header("location:../Posts/post_selected.php?getPostID=".$_POST['id_linkpost']);      
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
